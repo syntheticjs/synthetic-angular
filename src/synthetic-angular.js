c3a95ad81c224d679af169972e583f5f53bed258
morulus <vladimirmorulus@gmail.com>
2015-09-10 02:32:06 +0300

    ok

--- src/synthetic-angular.js
+++ src/synthetic-angular.js
@@ -1,11 +1,24 @@
 define([
-	"./template.html"
+	"./template.html",
+	"./template2.html"
 ],
-	function(tpl) {
+	function(tpl, tpl2) {
 	
 	Synthetic.createComponent('synthetic-angular', function($component) {
-		$component.created(function($self) {
+		$component.created(function($self, $scope) {
 			$self.template(tpl, 'angular');
+			
+		});
+
+		$component.watch('answers', ['response'], function(response) {
+			console.log('response is ', response);
+		});
+	});
+
+	Synthetic.createComponent('synthetic-angular2', function($component) {
+		$component.created(function($self, $scope) {
+			$self.template(tpl2, 'angular');
+			
 		});
 
 		$component.watch('answers', ['response'], function(response) {
